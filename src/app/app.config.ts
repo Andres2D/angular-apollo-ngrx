24eedf48e031c00f9b67c71830e528cfce31bbb3
Andres2D <andres2d1997.1206@gmail.com>
2024-05-15 13:11:23 -0400

    feat: add ngrx dependency

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,11 +4,13 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideApollo } from './providers/apollo.provider';
+import { provideStore } from '@ngrx/store';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideHttpClient(),
     provideApollo(),
     provideRouter(routes),
-  ]
+    provideStore()
+]
 };
